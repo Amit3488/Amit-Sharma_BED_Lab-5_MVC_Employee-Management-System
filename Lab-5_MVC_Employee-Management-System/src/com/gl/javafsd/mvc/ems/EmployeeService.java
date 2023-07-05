1205d6ab86a191c301d1722158774376741de99d
Amit3488 <124434800+Amit3488@users.noreply.github.com>
2023-07-05 06:49:46 +0530

    Update EmployeeService.java

--- Lab-5_MVC_Employee-Management-System/src/com/gl/javafsd/mvc/ems/EmployeeService.java
+++ Lab-5_MVC_Employee-Management-System/src/com/gl/javafsd/mvc/ems/EmployeeService.java
@@ -1,5 +1,11 @@
-package com.gl.javafsd.mvc.ems;
+package com.greatlearning.ems.service;
+import java.util.List;
+import com.greatlearning.ems.entity.Employee;
+public interface EmployeeService {
+	List<Employee> listAllEmployees();
+	void saveEmployee(Employee employee);
 
-public class EmployeeService {
+	Employee getEmployeeDetails(Long id);
 
+	void deleteEmployee(Long id);
 }
